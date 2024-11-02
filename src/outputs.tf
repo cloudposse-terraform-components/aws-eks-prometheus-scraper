8da108cc38a0d7cad527d7c736a36516d054c7d4
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:19:12 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,9 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "scraper_role_arn" {
+  description = "The Amazon Resource Name (ARN) of the IAM role that provides permissions for the scraper to discover, collect, and produce metrics"
+  value       = local.aps_clusterrole_username
+}
+
+output "clusterrole_username" {
+  description = "The username of the ClusterRole used to give the scraper in-cluster permissions"
+  value       = local.aps_clusterrole_identity
 }
